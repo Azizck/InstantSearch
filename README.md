84a4eed0c81ced7261d6b09c6fde6b9aca6e7078
Aziz Omar <46901810+Azizck@users.noreply.github.com>
2021-02-07 22:26:52 -0500

    Add files via upload
    Init

---
+++ README.md
@@ -0,0 +1,114 @@
+﻿# Insta-Search
+
+### What is instant serach ? 
+
+Instant search provides Search & Re-order as you type function using only plain javascript with keeping user interface functional.  
+It searches through hundreds of elements in less than 0.5s without freezing the browser and it places the top matching elemetns to the top with the keyword  highlighted.
+
+
+
+
+
+
+#### When to use instant search ? 
+ Need a very fast searching function <br />
+ Realying on front-end for search
+
+### Could Instant Search be used with a large data e.g 15K records? 
+If your device browser could render the large data, Instant search will work fine! Although we recommend using Instant Search to maximum of 1000 records. 
+
+
+### How to use Instant Search ? 
+
+Instant search is just a javascript class. It takes an **object** as parameter. Parameters are optional if required classes exist in DOM. Each record must have  **data-id** attribute with a unique value <br /> 
+
+<a href="https://azizomarck.github.io/liveSearch/demo/demo.html" target="_blank">Example - Dynamic data</a><br />
+<a href="https://azizomarck.github.io/liveSearch/example/example.html" target="_blank">Simple Example - Quick Start</a> 
+```html
+	<script src="./search.js"></script>
+	<body>
+		<input id="find">
+			<!-- container -->
+			<div id="employeesList">
+				<!--record-->
+				<!--data-id is required and must be unique-->
+				<section class="search-item" data-id="9021">
+					<div class="field"> John Smoith</div>
+					<div>
+						<a>link to John profile</a>
+					</div>
+					<div class="field"> Accounting Department</div>
+				</section>
+				<!--record-->
+				<!--data-id is required and must be unique-->
+				<section class="search-item" data-id="9321">
+					<div class="field">Oscar Wilde</div>
+					<div>
+						<a>link to Oscar profile</a>
+					</div>
+					<div class="field">Marketing Department </div>
+				</section>
+			</div>
+		</body>
+		<script>
+  let instanceSearch = new InstantSearch({
+    containerSelector: "#employeesList"
+  })
+</script>
+```
+
+
+
+
+
+ <table>
+   <tr>
+      <th>Parameter Name</th>
+      <th>Type</th>
+      <th>Description</th>
+      <th>Default Value</th>
+   </tr>
+   <tr>
+      <td><code>containerSelector</code></td>
+      <td>css selector | string</td>
+      <td>selector for the element that wraps all the records</td>
+      <td><code>"#result"</code></td>
+   </tr>
+   <tr>
+      <td><code>searchInputSelector</code></td>
+      <td>css selector | string</td>
+      <td>selecotor for the input element where users types a keyword to search for</td>
+      <td><code>"#find"</code></td>
+   </tr>
+   <tr>
+      <td><code>searchItemSelector</code></td>
+      <td>css selector | string</td>
+      <td> selector for the element that contains the record </td>
+      <td><code>".search-item"</code></td>
+   </tr>
+   <tr>
+      <td><code>fieldSelector</code></td>
+      <td>css selector | string</td>
+      <td>selector for the element that contains the record text</td>
+      <td><code>".field"</code></td>
+   </tr>
+   <tr>
+      <td><code>resultCount</code></td>
+      <td> integer</td>
+      <td>number of records that will be dragged to the top if they contain the search keyword</td>
+      <td><code>10</code></td>
+   </tr>
+   <tr>
+      <td><code>highlightColor</code></td>
+      <td>hexcolor | string</td>
+      <td>background of the matching keyword</td>
+      <td><code>"#ffeb3b"</code></td>
+   </tr>
+   <tr>
+      <td><code>disableStyle</code></td>
+      <td>bool</td>
+      <td>if set to true styling will not be applied</td>
+      <td><code>false</code></td>
+   </tr>
+</table>
+
---
+++ search.js
@@ -0,0 +1,159 @@
+
+
+class Field {
+            constructor(id, name, text) {
+                this.id = id,
+                    this.name = name,
+                    this.text = text
+            }
+        }
+        class InstantSearch {
+            constructor({
+            containerSelector = "#result",
+            searchInputSelector= "#find",
+            searchItemSelector = ".search-item",
+            fieldSelector = ".field",
+            resultCount = 10,
+            highlightColor = "#ffeb3b" ,
+            disableStyle = false
+            }){
+             this.searchInputSelector=searchInputSelector,
+             this.containerSelector = containerSelector,
+             this.searchItemSelector = searchItemSelector,
+             this.fieldSelector = fieldSelector,
+             this.resultCount = resultCount,
+             this.highlightColor = highlightColor,
+             this.disableStyle = disableStyle,
+                 this._searchData = []
+                if (this.ValidElements()){
+                    this.Init()
+                }
+            }
+
+            ValidElements() {
+                if(typeof this.containerSelector != 'string' ||
+                 typeof this.searchInputSelector != "string" ||
+                 typeof this.searchItemSelector != "string" ||
+                 typeof this.fieldSelector != "string") {
+                 console.log('Param passed is not a vlaid css selector')
+                 return false;
+                 }
+                 let messages = [];
+                if (document.querySelector(this.containerSelector) == null)
+                    messages.push('Instant Search: invalid selector for container element ' + this.containerSelector);
+                if (document.querySelector(this.searchInputSelector) == null)
+                    messages.push('Instant Search: invalid selector ' + this.searchItemSelector)
+                if (document.querySelector(this.searchItemSelector) == null)
+                    messages.push('Instant Search: invalid selector ' + this.searchItemSelector)
+                if(document.querySelector('[data-id]') == null)
+                messages.push('search item element must have "data-id" attribute')
+               
+                if(messages.length > 0){
+                    messages.forEach(e=>{console.log(e)})
+                }
+                return messages.length < 1
+
+
+            }
+
+            FormMark(e) {
+                return `<span class="mark">${e}</span>`
+            }
+
+            GetSearchData() {
+                let searchItems = Array.from(document.querySelectorAll(
+                    `${this.containerSelector} > ${this.searchItemSelector}`));
+                searchItems.forEach((element) => {
+                    let value = element.innerText;
+                    let id = element.getAttribute('data-id');
+                    let item = {
+                        value,
+                        id
+                    };
+                    this._searchData.push(item);
+                })
+
+            }
+            _SetStyle() {
+                document.querySelector(this.searchInputSelector).place
+                var style = document.createElement('style');
+                style.type = 'text/css';
+                if(!this.disableStyle){
+                style.innerHTML += `
+                ${this.searchInputSelector} {
+                    width: 80%;
+                    padding: 2.rem;
+                    padding: 0.8rem;
+                    outline: gray;
+                    border: 1px solid #d6d4d4;
+                    font-size: 1.1em;
+                    margin: 10px; 
+                    display:block;
+                    margin:auto;
+                    background: #e6eaed; 
+                } `;
+            style.innerHTML += 
+            `${this.searchItemSelector}{
+                padding: 10px;
+                background-color: white;
+                margin: 10px;
+                box-shadow: 3px 5px 7px -8px grey;
+            }`;
+
+            style.innerHTML += 
+            `${this.containerSelector}{
+                background-color: #fafbfd;
+                padding: 10px;
+            }`;
+        }
+                style.innerHTML += `.mark{ background-color:${this.highlightColor} !important; }`;
+                document.getElementsByTagName('head')[0].appendChild(style);
+            }
+            Find(keyword) {
+                var self = this;
+                Array.from(document.getElementsByClassName('mark')).forEach(e => e.classList.remove('mark'));
+                let regExp = new RegExp(keyword, 'ig');
+                let totalResult = self._searchData.filter(e => e.value.match(regExp)!= null?e.value.match(regExp).length > 0:false);
+                if (!totalResult)
+                    return false;
+               
+
+                let result = totalResult.splice(0, self.resultCount);
+
+                result.forEach(e => {
+                    setTimeout(() => {
+                        let element = document.querySelector(`*[data-id="${e.id}"]`);
+                        let elemenText = element.textContent.match(regExp)[0];
+                        if (elemenText == undefined || elemenText == null)
+                            return false;
+                        // element.innerHTML = element.innerText.replaceAll(regExp, self.FormMark( matchText));
+                        // document.querySelector(this.containerSelector).prepend(element);
+                        // iterate though the fields  get the matched string and replace
+                        this.fields = Array.from(document.querySelectorAll(
+                            `*[data-id="${e.id}"] ${this.fieldSelector}`));
+                        // console.log(this.fields);
+                        this.fields.forEach(e => {
+                            let fieldElement = e;
+                            let matchText = fieldElement.textContent.match(regExp)?.[0];
+                            fieldElement.innerHTML = fieldElement.innerText.replaceAll(
+                                regExp, self.FormMark(matchText));
+                            document.querySelector(this.containerSelector).prepend(element);
+                        });
+                    }, 0)
+
+                })
+
+            }
+            Init() {
+                this._SetStyle();
+                this.GetSearchData();
+                // element that holds the user input
+                let input = document.querySelector(this.searchInputSelector)
+                var self = this;
+                input.addEventListener('input', function () {
+                    self.Find(this.value);
+                })
+            }
+
+
+        }
